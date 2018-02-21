7ddbaa21fad2f65eae939a599e10d623a57781b3
wescrib <scribner.william@gmail.com>
2018-02-21 07:44:48 -0500

    added live validation to front end. password val still needs work

--- LOGIN_TEMPLATE_ANGULAR/public/src/app/user/user-login/user-login.component.ts
+++ LOGIN_TEMPLATE_ANGULAR/public/src/app/user/user-login/user-login.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { UserService } from '../../server/controllers/user.service';
 import { User } from '../../server/models/user';
 import { Router } from '@angular/router';
+// import { FormGroup } from '@angular/forms/src/model';
+// import { Validators } from '@angular/forms/src/validators';
+// import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-user-login',
@@ -11,22 +14,32 @@ import { Router } from '@angular/router';
 export class UserLoginComponent implements OnInit {
   private _userService: UserService;
   private user: User;
-  private _router: Router
-  errors: string[] = []
+  private _router: Router;
+  errors: string[] = [];
+  // private testForm: FormGroup;
 
   constructor(
     _userService: UserService,
-    _router: Router
+    _router: Router,
+    // testForm: FormGroup
   ) {
     this._userService = _userService;
     this.user = new User;
     this._router = _router;
+    // this.testForm = testForm;
 
    }
 
-  ngOnInit() {
-    // this.inSession()
-  }
+  ngOnInit(){}
+  // : void {
+  //   this.testForm = new FormGroup({
+  //     "lName": new FormControl(this.user.last_name,[
+  //       Validators.required,
+  //       Validators.minLength(2)
+  //     ]),
+  //   }
+  // )}
+
   loginUser(){
     console.log("login info hit sub component",this.user)
     this._userService.login(this.user, (res) => {
